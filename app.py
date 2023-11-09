ea33d7827c261a006a8240037957793068eb3367
NamJungHyun <nam9490@gmail.com>
2023-11-09 16:41:27 +0900

    .

--- app.py
+++ app.py
@@ -26,8 +26,6 @@ def remove_member():
     member_name = request.form['member_name']
     if member_name in members:
         del members[member_name]
-	print(Hello)
-	print(Good Bye)
     return redirect(url_for('index'))
 
 @app.route('/add_book', methods=['POST'])
